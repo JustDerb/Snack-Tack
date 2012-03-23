23fee57c6be99e168425589281a083f20931b55b
Justin Derby <justderb@iastate.edu>
2012-03-23 11:58:28 -0500

    Took out require comment

--- api/arrays.php
+++ api/arrays.php
@@ -14,9 +14,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
-//require_once "file.php";
-
+ 
 /**
  * Abstract array object that gives common functions for the array
  */
