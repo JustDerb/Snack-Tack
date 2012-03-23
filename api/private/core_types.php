40cf2f8eef1d1ee965a2e66173100078a5d62d3f
Justin Derby <justderb@iastate.edu>
2012-03-23 12:25:01 -0500

    Added types template

---
+++ api/private/core_types.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright 2012 Justin Derby
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+ 
+ 
+
+require_once 'includes/fbsdk/facebook.php';
+require_once 'api/arrays.php';
+require_once 'api/private/sql_functions.php';
+
+
+function st_types_getList()
+{
+
+}
+
+function st_types_search($name)
+{
+
+}
+
+function st_types_addType($name, $description)
+{
+
+}
+
+function st_types_deleteTypes($typeID)
+{
+
+}
+
+
+?>
