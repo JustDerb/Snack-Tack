5feeb399ffb675892efd15eec8701863a865ca03
Justin Derby <justderb@iastate.edu>
2012-04-02 21:54:45 -0500

    Test mobile site

--- mobile.php
+++ mobile.php
@@ -3,78 +3,55 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>
-        </title>
-        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
-        <style>
-            /* App custom styles */
-        </style>
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js">
-        </script>
-        <script src="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js">
-        </script>
+        <title>Snack Tack</title>
+        <link rel="stylesheet" href="css/reset.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="css/justin_mobile.css" type="text/css" media="screen" />
     </head>
     <body>
-        <div data-role="page" id="page1">
-            <div data-theme="a" data-role="header">
-                <h3>
-                    Snack Tack
-                </h3>
-            </div>
-            <div data-role="content">
-            	<ul data-role="listview" data-divider-theme="b" data-inset="true">
-					<li data-theme="c">
-                        <a href="#page1" data-transition="slide">
-                            Login with Facebook
-                        </a>
-                    </li>
-				</ul>
-                <ul data-role="listview" data-divider-theme="b" data-inset="true">
-                    <li data-role="list-divider" role="heading">
-                        Snack
-                    </li>
-                    <li data-theme="c">
-                        <a href="#page1" data-transition="slide">
-                            Plan
-                        </a>
-                    </li>
-                    <li data-theme="c">
-                        <a href="#page1" data-transition="slide">
-                            Find
-                        </a>
-                    </li>
-                    <li data-theme="c">
-                        <a href="#page1" data-transition="slide">
-                            Promote
-                        </a>
-                    </li>
-                </ul>
-                <ul data-role="listview" data-divider-theme="b" data-inset="true">
-                    <li data-theme="c">
-                        <a href="#page1" data-transition="slide">
-                            About
-                        </a>
-                    </li>
-                    <li data-theme="c">
-                        <a href="#page1" data-transition="slide">
-                            Privacy
-                        </a>
-                    </li>
-                    <li data-theme="c">
-                        <a href="#page1" data-transition="slide">
-                            Terms
-                        </a>
-                    </li>
-                    <li data-theme="c">
-                        <a href="#page1" data-transition="slide">
-                            Support
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <script>
-            //App custom javascript
-        </script>
+    	<div class="nav nav-left">
+    		<div class="nav-content">
+    			&lt; Left
+    		</div>
+    	</div>
+    	<div class="nav nav-right">
+    		<div class="nav-content">
+    			Right &gt;
+    		</div>
+    	</div>
+    	<div class="header clearfix">
+    		<div class="container clearfix">
+    			Snack Tack
+    		</div>
+    	</div>
+    	
+    	<ul>
+    	<?php
+    		function print_useless()
+    		{
+    			print '<li>
+		    		<div class="event-description-wrapper">
+						<div class="event-description">
+							<div class="innertube">
+								<p id="event-info">This is a generic description of the event...</p>
+								<p id="event-details">Type: Pizza | Where: 1020 Coover Hall | $1/slice ($1 pop)</p>
+							</div>
+						</div>
+					</div>
+					<div class="event-picture">
+						
+					</div>
+					<div class="clear"></div>
+	    		</li>';
+    		}
+    	
+    		for ($i=0; $i<20; $i++)
+    		{
+    			print_useless();
+    		}
+    	?>
+    	</ul>
+    	
+    	<div class="footer">
+    	</div>
     </body>
 </html>
