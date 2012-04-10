cc86ae96f97266c127a72dcc19211736e72a0710
Justin Derby <justderb@iastate.edu>
2012-04-10 15:56:49 -0500

    API Layout facelift

--- test/awards.php
+++ test/awards.php
@@ -51,60 +51,81 @@ if ($user) {
 }
 
 ?>
-<!doctype html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<title>SnackTack | API Testing Ground</title>
-	<style>
-		body {
-		font-family: 'Lucida Grande', Verdana, Arial, sans-serif;
-		}
-		h1 a {
-		text-decoration: none;
-		color: #3b5998;
-		}
-		h1 a:hover {
-		text-decoration: underline;
-		}
-	</style>
+	<title>SnackTack API Testing Ground</title>
+	<link rel="stylesheet" href="css/testing.css" type="text/css" media="all" />
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+	<meta name="MSSmartTagsPreventParsing" content="TRUE" />
+	<meta http-equiv="expires" content="-1" />
+	<meta http-equiv= "pragma" content="no-cache" />
 </head>
 <body>
-	<h3>GET</h3>
-	<pre><?php print_r($_GET); ?></pre>
-	<h3>POST</h3>
-	<pre><?php print_r($_POST); ?></pre>
-	
-	<?php
-		if ($_POST['form'] == 'saveUserData')
-		{
-		}
-	?>
-
-	<h1>SnackTack API Testing Ground</h1>
-	<p><a href="index.php">User</a> | <a href="events.php">Events</a> | <a href="awards.php">Awards</a></p>
-	
-	<?php if ($user): ?>
-		<p><?php 
-			$st_user = st_user_getData($user_profile['id']);
-			if ($user != NULL)
-				print("You are already registered!");
-			else
-			{
-				$st_user = st_user_register($user_profile); 
-				print("Congratulations! You have been registered!");
-			}
-		?></p>
-		<a href="<?php echo $logoutUrl; ?>">Logout</a>
-	<?php else: ?>
-		<div>
-			Login using OAuth 2.0 handled by the PHP SDK: <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
-		</div>
-	<?php endif ?>
-	
-	<?php if ($user): ?>
+<div id="top">
+	<div style="float:left">
+		<h1>SnackTack API Testing Ground - Awards</h1>
+	</div>
+	<div style="float:right">
+		<?php if ($user): ?>
+			<p><?php 
+				$st_user = st_user_getData($user_profile['id']);
+				if ($user != NULL)
+					print("You are already registered!");
+				else
+				{
+					$st_user = st_user_register($user_profile); 
+					print("Congratulations! You have been registered!");
+				}
+			?></p>
+			<a href="<?php echo $logoutUrl; ?>">Logout</a>
+		<?php else: ?>
+			<div>
+				Login using OAuth 2.0 handled by the PHP SDK: <a href="<?php echo $loginUrl; ?>">
+				Login with Facebook</a>
+			</div>
+		<?php endif ?>
+	</div>
+	<div style="clear:both"></div>
+</div>
+<div id="left">
+	<pre><a href="index.php">User</a></pre>
+	<pre><a href="events.php">Events</a></pre>
+	<pre><a href="awards.php">Awards</a></pre>	
+</div>
+<?php if ($user): ?>
+	<div id="middle">
 		<h2>Testing Features</h2>
 		<h3>Awards</h3>
 		<pre><?php print_r(st_award_getAll($st_user->array['ID'])); ?></pre>
-	<?php endif ?>
+	</div>
+	<div id="middle">
+		<?php if ($_GET['me']): ?>
+			<p><a href="?me=0">Hide /me data</a></p>
+			<h3>Your User Object (/me)</h3>
+			<pre><?php print_r($user_profile); ?></pre>
+			<h3>Your networks</h3>
+			<pre><?php 
+				print_r(st_user_getNetworks($user, $facebook)); 
+			?></pre>
+			<p><a href="?me=0">Hide /me data</a></p>
+		<?php else: ?>
+			<p><a href="?me=1">Show /me data</a></p>
+		<?php endif ?>
+	</div>
+<?php else: ?>
+	<div id="middle">
+		<p>Please login to access testing features.</p>
+	</div>
+<?php endif ?>
+<div id="right">
+	<h2 style="text-align:center">PHP Stuff</h2>
+	<h3>GET</h3>
+	<pre><?php print_r($_GET); ?></pre>
+	<h3>POST</h3>
+	<pre><?php print_r($_POST); ?></pre>	
+	<img src="https://graph.facebook.com/<?php echo $user; ?>/picture" alt=""/><h2><?php print($user_profile['name']); ?></h2>
+	<h3>User: <?php echo $user; ?></h3>
+</div>
 </body>
 </html>
