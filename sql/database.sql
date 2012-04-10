809eb2e438208ad1edd020b6d302ee7f63cb702b
Justin Derby <justderb@iastate.edu>
2012-04-09 20:37:32 -0500

    Updated structure of SQL table

--- sql/database.sql
+++ sql/database.sql
@@ -1,11 +1,106 @@
 use snacktack;
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `awards`;
+CREATE TABLE IF NOT EXISTS `awards` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `description` text NOT NULL,
+  `icon` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
+DROP TABLE IF EXISTS `eventcategory`;
+CREATE TABLE IF NOT EXISTS `eventcategory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+DROP TABLE IF EXISTS `eventratings`;
+CREATE TABLE IF NOT EXISTS `eventratings` (
+  `eventid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `rating` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `description` longtext NOT NULL,
+  `dateStart` datetime NOT NULL,
+  `dateEnd` datetime NOT NULL,
+  `location` int(11) NOT NULL,
+  `organization` int(11) DEFAULT NULL,
+  `fbEvent` text,
+  PRIMARY KEY (`id`),
+  KEY `LocationInx` (`location`),
+  KEY `ownerIdx` (`owner`),
+  KEY `orgIdx` (`organization`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `eventtypes`;
+CREATE TABLE IF NOT EXISTS `eventtypes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `category` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Category` (`category`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+DROP TABLE IF EXISTS `locations`;
+CREATE TABLE IF NOT EXISTS `locations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `networkid` varchar(30) NOT NULL,
+  `name` text NOT NULL,
+  `room` varchar(10) NOT NULL,
+  `address` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `organizations`;
+CREATE TABLE IF NOT EXISTS `organizations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ownerid` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `url` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ownerId` (`ownerid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `userawards`;
+CREATE TABLE IF NOT EXISTS `userawards` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` int(11) NOT NULL,
+  `awardid` int(11) NOT NULL,
+  `received` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userAward` (`user`,`awardid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fbID` varchar(30) NOT NULL,
+  `fbid` varchar(30) NOT NULL,
   `registered` datetime NOT NULL,
   `phone` varchar(20) DEFAULT NULL,
+  `networkid` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+
+
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`Location`) REFERENCES `locations` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `eventtypes`
+  ADD CONSTRAINT `eventtypes_ibfk_1` FOREIGN KEY (`Category`) REFERENCES `eventcategory` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `organizations`
+  ADD CONSTRAINT `organizations_ibfk_1` FOREIGN KEY (`ownerId`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE `userawards`
+  ADD CONSTRAINT `userawards_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
