30683995cbdcc979c512d2abd8f8d192412f3669
Justin Derby <justderb@iastate.edu>
2012-04-10 20:59:02 -0500

    New header (With JQuery)

---
+++ test/includes/header.php
@@ -0,0 +1,7 @@
+<title>SnackTack API Testing Ground</title>
+<link rel="stylesheet" href="css/testing.css" type="text/css" media="all" />
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<meta name="MSSmartTagsPreventParsing" content="TRUE" />
+<meta http-equiv="expires" content="-1" />
+<meta http-equiv= "pragma" content="no-cache" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
