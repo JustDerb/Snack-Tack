45b485ddf7fe3c2699e84d37399c3281560d9df4
Cameron <cameron@cameronl.student.iastate.edu>
2012-04-11 14:40:17 -0500

    Added section for viewing daily events and user-added events

---
+++ cam/today.php
@@ -0,0 +1,24 @@
+<?php require "includes/fb-login.php"; 
+	  require '../api/snacktack.php'; ?>
+<html>
+	<head>
+<?php require "includes/head.php"; ?>
+	</head>
+	<body>
+		<script type="text/javascript">// <![CDATA[ function BlockMove(event) { event.preventDefault(); } // ]]></script>
+		<div id="header">
+			<a href="index.php"><img src="img/logo_mini.png" id="logo" /></a>
+<?php if ($user): ?>
+			<a href="profile.php"><img src="https://graph.facebook.com/<?php echo $user; ?>/picture" alt="" id="fbPicture" /></a>
+<?php endif ?>
+		<div id="clear"></div>
+		</div>
+
+		<ul id="todaysEvents">
+			<li class="head">Today's Events</li>
+		</ul>
+
+		<div id="back" name="back" onclick="window.location.replace('index.php')">Back</div>
+<?php include "includes/labelfix.php"; ?>
+	</body>
+</html>
