ba41d4ed2d67dfcc04ea7dade2b7399e94c5a08d
Justin Derby <justderb@iastate.edu>
2012-04-11 20:20:07 -0500

    Updated page to reflect deletions

--- index.php
+++ index.php
@@ -9,10 +9,8 @@
 <body style="text-align:center">
 	<h1>Snack Tack</h1>
 	<div style="font-size:x-large">
-		<p><a href="example.php">OAuth Login With Facebook</a></p>
 		<p><a href="test/">API Testing</a></p>
-		<p><a href="cam/">Mobile (Cam)</a></p>
-		<p><a href="mobile.php">Mobile (Justin)</a></p>
+		<p><a href="cam/">Mobile Site</a></p>
 	</div>
 </body>
 </html>
