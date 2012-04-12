a6ec88c9348401b803037fd3b42fa8891dd0df6b
Justin Derby <justderb@iastate.edu>
2012-04-12 16:35:02 -0500

    v1.0 of PHP for creating event

--- actions/submitplan.php
+++ actions/submitplan.php
@@ -9,11 +9,57 @@
 		$form['msg']['success'] = array();
 		$changed_user_info = false;
 		
-		if (array_key_exists('phone', $_POST))
+		
+		if (!array_key_exists('eventName', $_POST) ||
+			!array_key_exists('eventDescription', $_POST) ||
+			!array_key_exists('foodOptions', $_POST) ||
+			!array_key_exists('month', $_POST) ||
+			!array_key_exists('date', $_POST) ||
+			!array_key_exists('year', $_POST) ||
+			!array_key_exists('startHour', $_POST) ||
+			!array_key_exists('startMinute', $_POST) ||
+			!array_key_exists('startAMPM', $_POST) ||
+			!array_key_exists('endHour', $_POST) ||
+			!array_key_exists('endMinute', $_POST) ||
+			!array_key_exists('endAMPM', $_POST) ||
+			!array_key_exists('location', $_POST) ||
+			!array_key_exists('fburl', $_POST))// ||
+			//!array_key_exists('organization', $_POST))
 		{
+			array_push($form['msg']['error'],'Missing some data to create event.  Please try again.');
 		}
-				
-					
-		array_push($form['msg']['message'],'<pre>'.print_r($_POST,true).'</pre>');
+		else
+		{
+			$event = new st_arr_event();
+    		$event->array["creatorID"] = $st_user->array['ID'];
+    		$event->array["NetworkID"] = $st_user->array['Network'];
+    		$event->array["Name"] = $_POST['eventName'];
+    		$event->array["Description"] = $_POST['eventDescription'];
+    		if ($_POST['foodOptions'])
+    		{
+	    		$types = $_POST['foodOptions'];
+	    		foreach ($types as $key => $value)
+	    		{
+	    			$event->array["Type"][] = $value;
+	    		}
+    		}
+    		$event->array["WhenStart"] = st_DateTime_getDateTime($_POST['year'],$_POST['month'],$_POST['date'],$_POST['startHour'],$_POST['startMinute'],$_POST['startAMPM'] == 'AM');
+    		$event->array["WhenEnd"] = st_DateTime_getDateTime($_POST['year'],$_POST['month'],$_POST['date'],$_POST['endHour'],$_POST['endMinute'],$_POST['endAMPM'] == 'AM');;
+    		$event->array["Location"] = $_POST['location'];
+    		$event->array["FacebookEvent"] = $_POST['fburl'];
+    		$event->array["Organization"] = "-1";//$_POST['organization'];	
+			
+			$result = st_events_createEvent($event);
+			
+			if ($result->array['Error'] == 1)
+				array_push($form['msg']['error'],$result->array['Message']);
+			else
+			{
+				if (!empty($result->array['Message']))
+					array_push($form['msg']['success'],$result->array['Message']);
+			}
+		}
+							
+		//array_push($form['msg']['message'],'<pre>'.print_r($_POST,true).'</pre>');
 	}
 ?>
