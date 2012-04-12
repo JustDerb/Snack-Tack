68a1a29c8fd100d200d4e45fb21cd4c2df15bba1
Justin Derby <justderb@iastate.edu>
2012-04-12 17:43:54 -0500

    PHP now updates forms to current time and holds values after submit

--- plan.php
+++ plan.php
@@ -64,7 +64,17 @@
 	$types = st_types_getList();
 	foreach($types as $type)
 	{
-		print('<li><input type="checkbox" name="foodOptions[]" id="'.$type->array['ID'].'" value="'.$type->array['ID'].'">');
+		print('<li><input type="checkbox" name="foodOptions[]" id="'.$type->array['ID'].'" value="'.$type->array['ID'].'"');
+		if ($_POST['foodOptions'])
+		{
+    		$types = $_POST['foodOptions'];
+    		foreach ($types as $key => $value)
+    		{
+    			if ($value == $type->array['ID'])
+    				print('checked');
+    		}
+		}
+		print('>');
 		print($type->array['Category'].' - '.$type->array['Name']);
 		print('</label></li>');
 	}
@@ -77,20 +87,60 @@
 					<select name="month">
 <?php 
 	$months = array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+	$current_month = Date("M");
 	foreach ($months as $month)
-		print("<option value=\"" . $month . "\">" . $month . "</option>");
+	{
+		print("<option value=\"" . $month . "\"");
+		if (array_key_exists('month', $_POST))
+		{
+			if (strcasecmp($_POST['month'],$month) == 0)
+				print("selected=\"selected\"");
+		}
+		else
+			if (strcasecmp($current_month, $month) == 0)
+				print("selected=\"selected\"");
+		print(">" . $month . "</option>");
+	}
 ?>
 					</select>
 					
 					<select name="date">
 <?php
+	$current_date = Date("j");
 	for ($i = 1; $i < 32; $i++)
-		print("<option value=\"" . $i . "\">" . $i . "</option>");
+	{
+		print("<option value=\"" . $i . "\"");
+		if (array_key_exists('date', $_POST))
+		{
+			if (strcasecmp($_POST['date'],$i) == 0)
+				print("selected=\"selected\"");
+		}
+		else
+			if (strcasecmp($current_date, $i) == 0)
+				print("selected=\"selected\"");
+		print(">" . $i . "</option>");
+	}
 ?>
 					</select>
 					
 					<select name="year">
-						<option value="2012">2012</option>
+<?php
+	$current_year = Date("Y");
+	for ($i = 0; $i <= 1; $i++)
+	{
+		$year = ($current_year+$i);
+		print("<option value=\"" . ($year) . "\"");
+		if (array_key_exists('year', $_POST))
+		{
+			if (strcasecmp($_POST['year'],($year)) == 0)
+				print("selected=\"selected\"");
+		}
+		else
+			if (strcasecmp($current_year, ($year)) == 0)
+				print("selected=\"selected\"");
+		print(">" . ($year) . "</option>");
+	}
+?>
 					</select>
 				</li>
 			</ul>
@@ -101,76 +151,142 @@
 					Start
 					<select name="startHour">
 <?php
+	$current_hour = Date("g");
 	for ($i = 1; $i < 13; $i++)
 	{
 		print("<option value=\"" . $i . "\"");
-		if ($i == 12)
-			print(" selected=\"selected\"");
+		if (array_key_exists('startHour', $_POST))
+		{
+			if (strcasecmp($_POST['startHour'],$i) == 0)
+				print("selected=\"selected\"");
+		}
+		else
+			if (strcasecmp($current_hour, $i) == 0)
+				print("selected=\"selected\"");
 		print(">" . $i . "</option>");
 	}
 ?>
 					</select>
 					:
 					<select name="startMinute">
-						<option>00</option>
-						<option>10</option>
-						<option>15</option>
-						<option>30</option>
-						<option>45</option>
-						<option>50</option>
+<?php 
+	$minutes = array("00","10","15","30","45","50");
+	$current_minutes = Date("i");
+	foreach ($minutes as $minute)
+	{
+		print("<option value=\"" . $minute . "\"");
+		if (array_key_exists('startMinute', $_POST))
+		{
+			if (strcasecmp($_POST['startMinute'],$minute) == 0)
+				print("selected=\"selected\"");
+		}
+		//else
+		//	if (strcasecmp($current_minutes, $minute) == 0)
+		//		print("selected=\"selected\"");
+		print(">" . $minute . "</option>");
+	}
+?>
 					</select>
 					&nbsp;&nbsp;
 					<select name="startAMPM">
-						<option>AM</option>
-						<option selected="selected">PM</option>
+<?php
+	$AMPM = array("AM","PM");
+	$current_AMPM = Date("A");
+	foreach ($AMPM as $AMPM2)
+	{
+		print("<option value=\"" . $AMPM2 . "\"");
+		if (array_key_exists('startAMPM', $_POST))
+		{
+			if (strcasecmp($_POST['startAMPM'],$AMPM2) == 0)
+				print("selected=\"selected\"");
+		}
+		else
+			if (strcasecmp($current_AMPM, $AMPM2) == 0)
+				print("selected=\"selected\"");
+		print(">" . $AMPM2 . "</option>");
+	}
+?>
 					</select>
 				</li>
 				<li>
 					End&nbsp;
 					<select name="endHour">
 <?php
+	$current_hour = Date("g");
 	for ($i = 1; $i < 13; $i++)
 	{
 		print("<option value=\"" . $i . "\"");
-		if ($i == 12)
-			print(" selected=\"selected\"");
+		if (array_key_exists('endHour', $_POST))
+		{
+			if (strcasecmp($_POST['endHour'],$i) == 0)
+				print("selected=\"selected\"");
+		}
+		else
+			if (strcasecmp($current_hour, $i) == 0)
+				print("selected=\"selected\"");
 		print(">" . $i . "</option>");
 	}
 ?>
 					</select>
 					:
 					<select name="endMinute">
-						<option>00</option>
-						<option>10</option>
-						<option>15</option>
-						<option>30</option>
-						<option>45</option>
-						<option>50</option>
+<?php 
+	$minutes = array("00","10","15","30","45","50");
+	$current_minutes = Date("i");
+	foreach ($minutes as $minute)
+	{
+		print("<option value=\"" . $minute . "\"");
+		if (array_key_exists('endMinute', $_POST))
+		{
+			if (strcasecmp($_POST['endMinute'],$minute) == 0)
+				print("selected=\"selected\"");
+		}
+		//else
+		//	if (strcasecmp($current_minutes, $minute) == 0)
+		//		print("selected=\"selected\"");
+		print(">" . $minute . "</option>");
+	}
+?>
 					</select>
 					&nbsp;&nbsp;
 					<select name="endAMPM">
-						<option>AM</option>
-						<option selected="selected">PM</option>
+<?php
+	$AMPM = array("AM","PM");
+	$current_AMPM = Date("A");
+	foreach ($AMPM as $AMPM2)
+	{
+		print("<option value=\"" . $AMPM2 . "\"");
+		if (array_key_exists('endAMPM', $_POST))
+		{
+			if (strcasecmp($_POST['endAMPM'],$AMPM2) == 0)
+				print("selected=\"selected\"");
+		}
+		else
+			if (strcasecmp($current_AMPM, $AMPM2) == 0)
+				print("selected=\"selected\"");
+		print(">" . $AMPM2 . "</option>");
+	}
+?>
 					</select>					
 				</li>
 			</ul>
 
 			<ul>
 				<li class="head">Place</li>
-				<li><input type="text" placeholder="Event Location" name="location" id="location" autocapitalizer="on" autocorrect="off" autocomplete="off" /></li>
+				<li><input type="text" placeholder="Event Location" name="location" id="location" autocapitalizer="on" autocorrect="off" autocomplete="off" value="<?php if ($_POST['location']) print($_POST['location']); ?>" /></li>
 			</ul>
 
 			<ul>
 				<li class="head">Facebook Event URL</li>
-				<li><input type="text" placeholder="(Optional)" name="fburl" id="fburl" autocapitalizer="off" autocorrect="off" autocomplete="off" /></li>
+				<li><input type="text" placeholder="(Optional)" name="fburl" id="fburl" autocapitalizer="off" autocorrect="off" autocomplete="off" value="<?php if ($_POST['fburl']) print($_POST['fburl']); ?>" /></li>
 			</ul>
 			<!-- Put in later
 			<ul>
 				<li class="head">Organization</li>
-				<li><input type="text" placeholder="Name of Organization" name="organization" id="organization" autocapitalizer="on" autocorrect="off" autocomplete="off" /></li>
+				<li><input type="text" placeholder="Name of Organization" name="organization" id="organization" autocapitalizer="on" autocorrect="off" autocomplete="off" value="<?php if ($_POST['organization']) print($_POST['organization']); ?>" /></li>
 			</ul>
 			-->
-			
+			<input type="hidden" name="form" value="plan" />
 			<div id="submit" name="submit" onclick="return validateForm()">Submit</div>
 			<div id="cancel" name="cancel" onclick="window.location.replace('index.php')">Cancel</div>
 		</form>
