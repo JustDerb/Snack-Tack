15b839026b6fca62c60cbb8d5c76177fd692f458
Justin Derby <justderb@iastate.edu>
2012-04-12 22:41:38 -0500

    PHP included so it works!

--- eventinfo.php
+++ eventinfo.php
@@ -1,5 +1,15 @@
 <?php require "includes/fb-login.php"; 
-	  require "api/snacktack.php"; ?>
+	  require "api/snacktack.php"; 
+	  
+	$event = NULL;
+	if ($_GET['id'])
+	{
+		$event = st_events_getEvent($_GET['id']);
+		
+		if ($event->array['ID'] < 0)
+			$event = NULL;
+	}
+?>
 <html>
 	<head>
 <?php require "includes/head.php"; ?>
@@ -15,35 +25,59 @@
 		</div>
 
 		<h2>Event Information</h2>
+<?php if ($event): ?>
 		<ul class="info">
 			<li class="head">Basic Info</li>
-			<li>Name</li>
-			<li>Description</li>
+			<li><?php print($event->array['Name']); ?></li>
+			<li><?php print($event->array['Description']); ?></li>
 		</ul>
 
 		<ul class="info">
 			<li class="head">What's being sold</li>
-<?php //foreach all food options - don't include "Food - " part ?>
+<?php 
+			//foreach all food options - don't include "Food - " part 
+	$types = $event->array['Type'];
+	foreach ($types as $typeid)
+	{
+		$type = st_types_getType($typeid);
+		print('<li>'.($type->array['Name']).' ('.$type->array['Description'].')</li>');
+	}
+?>
 		</ul>
 
 		<ul class="info">
 			<li class="head">Day and Time</li>
-			<li>Thursday, April 12th 2012</li>
-			<li>
-				Start @ HH:MM
-				End @ HH:MM
-			</li>
+<?php
+	print('<li>'.($event->array["WhenStart"]->format("l, F j Y")).'</li>');
+	print('<li><strong>Start</strong> @ '.($event->array['WhenStart']->format('g:ia')));
+	print(' | <strong>End</strong> @ '.($event->array['WhenEnd']->format('g:ia')).'</li>');
+?>
+		</ul>
+		
+		<ul class="info">
+			<li class="head">Location</li>
+			<li><?php print($event->array['Location']); ?></li>
 		</ul>
 
+<!--
 		<ul class="info">
 			<li class="head">Organization</li>
-			<li>Com Sci and Soft E Club</li>
+			<li>Not done!</li>
 		</ul>
-
+-->
+<?php if (!empty($event->array['FacebookEvent'])): ?>
 		<ul class="info">
 			<li class="head">Facebook Event URL</li>
-			<li><a href="#">To the Facebook!</a></li>
+			<li><a href="<?php print($event->array['FacebookEvent']); ?>">To the Facebook!</a></li>
 		</ul>
+<?php endif ?>		
+
+<?php else: ?>
+		<ul class="info">
+			<li class="head">Uh-Oh!</li>
+			<li>This is an invalid event or it has been deleted!</li>
+		</ul>
+<?php endif ?>
 		
 <?php if (isset($_GET['home'])): ?>
 		<div id="back" name="back" onclick="index.php">Home</div>
