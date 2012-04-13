021f85ddd6cdc9c1f7fcd41301a1018c7b4a4639
Justin Derby <justderb@iastate.edu>
2012-04-12 19:55:21 -0500

    Basic searching functionality

--- find.php
+++ find.php
@@ -3,6 +3,7 @@
 <html>
 	<head>
 <?php require "includes/head.php"; ?>
+		<script type="text/javascript" src="js/find.js"></script>
 	</head>
 	<body>
 		<script type="text/javascript">// <![CDATA[ function BlockMove(event) { event.preventDefault(); } // ]]></script>
@@ -14,22 +15,35 @@
 			<div id="clear"></div>
 		</div>
 
-		<form>
+		<form method="get" id="findForm">
 			<ul>
 				<li class="head">Search Options</li>
-				<li><input type="text" placeholder="Search Terms" name="searchTerms" id="searchTerms" autocapitalizer="on" autocorrect="off" autocomplete="off" />
+				<li><input type="text" placeholder="Search Terms" name="terms" id="searchTerms" autocapitalizer="on" autocorrect="off" autocomplete="off" value="<?php if ($_GET['terms']) print($_GET['terms']); ?>"/>
+				<div id="submit" name="submit" onclick="return validateForm();">Submit</div>
+				<!--
 				<li><input type="radio" name="searchOption" value="byFoodType" id="byFoodType" checked /><label for="byFoodType">By Food Type</label></li>
-				<li><input type="radio" name="searchOption" value="byOrganization" id="byOrganization" /><label for="byOrganization">By Organization</li>
-				</li>
+				<li><input type="radio" name="searchOption" value="byOrganization" id="byOrganization" /><label for="byOrganization">By Organization</label></li>
+				-->
 			</ul>
 		</form>
 
-
+<?php if($_GET['terms']): ?>
 		<ul id="searchResults">
 			<li class="head">Search Results</li>
+<?php
+	$events = st_events_lookupEvent($_GET['terms'], 7, "date");
+	foreach($events as $event)
+	{
+		print('<li>');
+		print($event->array['Name']);
+		print('</li>');
+	}
+?>
 		</ul>
+<?php endif ?>
 
 		<div id="back" name="back" onclick="window.history.back();">Back</div>
 <?php include "includes/labelfix.php"; ?>
 	</body>
 </html>
+
