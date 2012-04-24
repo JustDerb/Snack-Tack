86ef2b48b60b7dfe9fc21e30fcb89f7ec4975436
Justin Derby <justderb@iastate.edu>
2012-04-23 22:22:07 -0500

    Added general description to promote page

--- promote.php
+++ promote.php
@@ -12,7 +12,18 @@
 	<body onload="setTimeout(function() { window.scrollTo(0, 1) }, 100);">
 <?php include "includes/header.php"; ?>
 
-
+		<ul class="message">
+			<li class="success">
+				The promote option is coming soon!
+			</li>
+			<li class="message">
+				Use this option to promote events that are not on Snack Tack, 
+				is not your event, but you want other people to know about it!
+				When the owner of the event sees your event, he or she can then
+				claim it as their own! (Also, your helping the Snack Tack community
+				and the event owners - a win-win!)
+			</li>
+		</ul>
 
 		<div id="back" name="back" onclick="window.history.back();">Back</div>	
 <?php include "includes/labelfix.php"; ?>
