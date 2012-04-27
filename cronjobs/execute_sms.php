725b580b5cde949c8c0898c836aea43352bf6ee0
Justin Derby <justderb@iastate.edu>
2012-04-26 20:56:42 -0500

    Start of cron job script

--- cronjobs/execute_sms.php
+++ cronjobs/execute_sms.php
@@ -7,4 +7,22 @@
 // Delete them from the table.  If there are any that are WAY before the current time, just delete them.  
 // Do not send a text.
 //
+require_once '../api/private/sql_functions.php';
+require_once '../api/google/googlevoice.php';
+
+//Get our records
+$gv = new GoogleVoice($googlevoice_email, $googlevoice_password);
+if ($token->array['Error'] == 0)
+{
+	//SELECT e.name,e.description,e.dateStart,e.dateEnd,t.userid,t.eventid FROM tacked t,users u,events e WHERE u.id=t.userid AND e.id=t.eventid AND t.userid=6
+	$gv->sms('PHONE', 'MESSAGE HERE');
+	//if ($gv->info['http_code'] != 200)
+		//Error sending!
+}
+else
+{
+	//Error authenticating!
+}
+
+
 ?>
