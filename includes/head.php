5930c609fa431132d9631651c322f12a2b1af521
Justin Derby <justderb@iastate.edu>
2012-04-27 16:13:23 -0500

    Took out OpenGraph for head.  Should be on every page since it changes so much.

--- includes/head.php
+++ includes/head.php
@@ -4,9 +4,5 @@
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 		<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1" />
 		<meta name="apple-mobile-web-app-capable" content="yes" />
-		<meta property="og:title" content="Snack Tack" >
-		<meta property="og:type" content="website" >
-		<meta property="og:url" content="http://www.wadsworthit.com/snacktack/" >
-		<meta property="og:image" content="http://www.wadsworthit.com/snacktack/img/logo_icon.png" >
 		<meta property="og:site_name" content="Snack Tack" >
-		<meta property="og:description" content="A convenient way of finding events on campus." >
+
