47b6f2484abc6ebf4f9882de6a6d01ebd3c3a693
Justin Derby <justderb@iastate.edu>
2012-06-30 15:44:13 -0700

    Added message to top of browser if the user is visiting from a desktop browser

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,18 @@
 		<script type="text/javascript">// <![CDATA[ function BlockMove(event) { event.preventDefault(); } // ]]></script>
+		
+		<?php
+			include 'includes/mobile_device_detect.php';
+			if (!@mobile_device_detect()) {
+				$notice = 
+<<<EOT
+		<div style="width:100%;background-color:#000000;color:#808080;text-align:center;font-size:1.5em;padding-bottom:0.5em;border-bottom:1px solid #FFFFFF">
+			<img src="img/types/icons/question.png" alt="" /> For now, we don't have a desktop version! (This site is better viewed on a tiny screen)
+		</div>
+EOT;
+				print($notice);
+			}
+		?>
+		
 		<div id="header">
 			<a href="index.php"><img src="img/logo_mini.png" id="logo" /></a>
 <?php if ($user): ?>
