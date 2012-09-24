273ca157419c7b52b2c38ed478e31c3b5ac4dad6
Justin Derby <justderb@iastate.edu>
2012-09-24 14:45:54 -0500

    Basic layout and javascript done

--- index2.php
+++ index2.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 		<title>SnackTack</title>
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<link href="css/desktop2.css" rel="stylesheet">
 		<script src="http://code.jquery.com/jquery-latest.js"></script>
 		<script src="js/bootstrap.min.js"></script>
+		<script src="js/desktop.js"></script>
 	</head>
 	<body>
-	    <div class="navbar navbar-static-top">
+	    <div class="navbar navbar-static-top" id="navbar-top">
 	    	<div class="navbar-inner">
 			    <div class="container-fluid" style="margin:10px 0">
 			    	<a href="index.php"><img src="img/logo_mini.png" id="logo" alt="SnackTack" /></a>
-		    	    <div class="btn-group pull-right">
-				    <button class="btn"><i class="icon-user"></i> Profile</button>
-				    <button class="btn dropdown-toggle" data-toggle="dropdown">
-				    <span class="caret"></span>
-				    </button>
-				    <ul class="dropdown-menu">
-				    	<li><a href="#"><i class="icon-bell"></i> Tacked Events</a></li>
-				    	<li><a href="#"><i class="icon-th-list"></i> My Planned Events</a></li>
-				    	<li><a href="#"><i class="icon-gift"></i> Awards</a></li>
-				    	<li><a href="#"><i class="icon-wrench"></i> Settings</a></li>
-				    	<li class="divider"></li>
-				    	<li><a href="#"><i class="icon-off"></i> Logout</a></li>
-				    </ul>
+		    	    <div class="btn-group pull-right spacer-small">
+					    <button class="btn"><i class="icon-user"></i> Profile</button>
+					    <button class="btn dropdown-toggle" data-toggle="dropdown">
+					    <span class="caret"></span>
+					    </button>
+					    <ul class="dropdown-menu">
+					    	<li><a href="#"><i class="icon-bell"></i> Tacked Events</a></li>
+					    	<li><a href="#"><i class="icon-th-list"></i> My Planned Events</a></li>
+					    	<li><a href="#"><i class="icon-gift"></i> Awards</a></li>
+					    	<li><a href="#"><i class="icon-wrench"></i> Settings</a></li>
+					    	<li class="divider"></li>
+					    	<li><a href="#"><i class="icon-off"></i> Logout</a></li>
+					    </ul>
 				    </div>
 			    	<form class="navbar-search pull-right">
 				    	<input type="text" class="search-query" placeholder="Search">
@@ -41,7 +43,7 @@
 								<tr>
 									<td colspan="3">
 									<img class="img-rounded" src="img/types/box.png" alt="Event Icon">
-									Justin 'Pepper' Derby
+									<h3 style="display:inline-table">Profile Name</h3>
 									</td>
 								</tr>
 							</thead>
@@ -63,15 +65,15 @@
 								<tr>
 									<td colspan="3">
 										<div class="btn-group">
-											<button class="btn btn-inverse" style="width:50%">Plan</button>
-											<button class="btn" style="width:50%">Promote</button>
+											<button class="btn btn-inverse" style="width:50%" id="tab-plan-btn">Plan</button>
+											<button class="btn" style="width:50%" id="tab-promote-btn">Promote</button>
 										</div>
 									</td>
 								</tr>
 							</tbody>
 						</table>
 					</div>
-					<div class="well well-small">
+					<div class="well well-small" id="tab-plan">
 					    <form>
 					    	<legend>Plan Event</legend>
 					    	<div class="input-prepend">
@@ -95,6 +97,49 @@
 								<input class="span11" type="text" name="eventURl" placeholder="Event URL...">
 							</div>
 							<button class="btn btn-large btn-inverse btn-block" type="submit">Plan</button>
+					    </form>
+					</div>
+					<div class="well well-small" id="tab-promote" style="display:none">
+					    <form>
+					    	<legend>Promote Event</legend>
+					    	<div class="input-prepend">
+								<span class="add-on"><i class="icon-exclamation-sign"></i></span>
+								<input class="span11" type="text" name="eventName" placeholder="Event Name...">
+							</div>
+							<div class="input-prepend">
+								<span class="add-on"><i class="icon-exclamation-sign"></i></span>
+								<textarea class="span11" name="eventDesc" placeholder="Event Description..." rows="3"></textarea>
+							</div>
+							<div class="input-prepend">
+								<span class="add-on"><i class="icon-exclamation-sign"></i></span>
+								<input class="span11" type="text" name="eventDateTime" placeholder="Day, Time, Duration...">
+							</div>
+							<div class="input-prepend">
+								<span class="add-on"><i class="icon-exclamation-sign"></i></span>
+								<input class="span11" type="text" name="eventLoc" placeholder="Location...">
+							</div>
+							<div class="input-prepend">
+								<span class="add-on"><i class="icon-exclamation-sign"></i></span>
+								<input class="span11" type="text" name="eventURl" placeholder="Event URL...">
+							</div>
+							<div class="dropdown">
+							<div class="input-prepend">
+								<span class="add-on"><i class="icon-exclamation-sign"></i></span>
+								<input class="span11" type="text" name="eventOwner" placeholder="Event Owner (Facebook search)">
+							</div>
+							    <ul class="dropdown-menu form-shifted" role="menu" aria-labelledby="dLabel">
+							    <li><a tabindex="-1" href="#">Action</a></li>
+							    <li><a tabindex="-1" href="#">Another action</a></li>
+							    <li><a tabindex="-1" href="#">Something else here</a></li>
+							    <li class="divider"></li>
+							    <li><a tabindex="-1" href="#">Separated link</a></li>
+							    </ul>
+						    </div>
+							<label class="checkbox form-shifted">
+								<input type="checkbox" value="">
+								This is not my event, but someone else's.
+							</label>							
+							<button class="btn btn-large btn-inverse btn-block" type="submit">Promote</button>
 					    </form>
 					</div>
 					<div class="snack-container"  style="text-align:center">
---
+++ js/desktop.js
@@ -0,0 +1,34 @@
+﻿$(function() {
+
+	// Enable the plan/promote tabs
+	$("#tab-plan-btn").click(function() {
+		$("#tab-plan").show();
+		$("#tab-promote").hide();
+		
+		$("#tab-plan-btn").addClass("btn-inverse");
+		$("#tab-promote-btn").removeClass("btn-inverse");
+	});
+	
+	$("#tab-promote-btn").click(function() {
+		$("#tab-plan").hide();
+		$("#tab-promote").show();
+		
+		$("#tab-plan-btn").removeClass("btn-inverse");
+		$("#tab-promote-btn").addClass("btn-inverse");
+	});
+	
+	var sideLocked = true;
+	$(window).bind('scroll', function(){
+	    var $nav = $('#navbar-top');
+	    if(sideLocked && ($(window).scrollTop() > ($nav.offset().top+$nav.height()))) {
+	    	sideLocked = false;
+	        //console.log('out');
+	        
+	    } else if(!sideLocked && ($(window).scrollTop() < ($nav.offset().top+$nav.height()))) {
+	    	sideLocked = true;
+	    	//console.log('in');
+	    }
+	});
+
+
+});
