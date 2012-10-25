2d89d76bf94320a40bda72d89bcb9e676104d92b
Cameron <cameron@cameronl-macbook.student.iastate.edu>
2012-10-25 13:16:25 -0500

    Migrated TOS and Help information to common code files

---
+++ common/tos.php
@@ -0,0 +1,156 @@
+<h2>Web Site Terms and Conditions of Use</h2>	
+	<ul class="terms info">
+		<li>1. Terms</li>
+		<li>
+			By accessing this web site, you are agreeing to be bound by these 
+			web site Terms and Conditions of Use, all applicable laws and regulations, 
+			and agree that you are responsible for compliance with any applicable local 
+			laws. If you do not agree with any of these terms, you are prohibited from 
+			using or accessing this site. The materials contained in this web site are 
+			protected by applicable copyright and trade mark law.
+		</li>
+	</ul>
+
+	<ul class="info terms">
+		<li>2. Use License</li>
+		<li>
+			Permission is granted to temporarily download one copy of the materials 
+			(information or software) on DBA Derby Legleiter Corporation's web site for personal, 
+			non-commercial transitory viewing only. This is the grant of a license, 
+			not a transfer of title, and under this license you may not:
+				<br />
+				<div class="indent">- modify or copy the materials;</div>
+				<div class="indent">- use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</div>
+				<div class="indent">- attempt to decompile or reverse engineer any software contained on DBA Derby Legleiter Corporation's web site;</div>
+				<div class="indent">- remove any copyright or other proprietary notations from the materials; or</div>
+				<div class="indent">- transfer the materials to another person or "mirror" the materials on any other server.</div>
+				<br />
+			This license shall automatically terminate if you violate any of these restrictions and may be terminated by DBA Derby Legleiter Corporation at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.
+		</li>
+	</ul>
+	
+	<ul class="info terms">
+		<li>3. Disclaimer</li>
+		<li>
+			The materials on DBA Derby Legleiter Corporation's web site are provided 
+			"as is". DBA Derby Legleiter Corporation makes no warranties, expressed or 
+			implied, and hereby disclaims and negates all other warranties, including 
+			without limitation, implied warranties or conditions of merchantability, 
+			fitness for a particular purpose, or non-infringement of intellectual property 
+			or other violation of rights. Further, DBA Derby Legleiter Corporation does 
+			not warrant or make any representations concerning the accuracy, likely results, 
+			or reliability of the use of the materials on its Internet web site or 
+			otherwise relating to such materials or on any sites linked to this site.
+		</li>
+	</ul>
+	
+	<ul class="info terms">
+		<li>4. Limitations</li>
+		<li>
+			In no event shall DBA Derby Legleiter Corporation or its suppliers be liable for any 
+			damages (including, without limitation, damages for loss of data or profit, or due 
+			to business interruption,) arising out of the use or inability to use the materials 
+			on DBA Derby Legleiter Corporation's Internet site, even if DBA Derby Legleiter 
+			Corporation or a DBA Derby Legleiter Corporation authorized representative has been 
+			notified orally or in writing of the possibility of such damage. Because some 
+			jurisdictions do not allow limitations on implied warranties, or limitations of 
+			liability for consequential or incidental damages, these limitations may not apply to you.
+		</li>
+	</ul>
+	
+	<ul class="info terms">
+		<li>5. Revisions and Errata</li>
+		<li>
+			The materials appearing on DBA Derby Legleiter Corporation's web site could include 
+			technical, typographical, or photographic errors. DBA Derby Legleiter Corporation 
+			does not warrant that any of the materials on its web site are accurate, complete, 
+			or current. DBA Derby Legleiter Corporation may make changes to the materials 
+			contained on its web site at any time without notice. DBA Derby Legleiter Corporation 
+			does not, however, make any commitment to update the materials.
+		</li>
+	</ul>
+	
+	<ul class="info terms">
+		<li>6. Links</li>
+		<li>
+			DBA Derby Legleiter Corporation has not reviewed all of the sites linked to its 
+			Internet web site and is not responsible for the contents of any such linked site. 
+			The inclusion of any link does not imply endorsement by DBA Derby Legleiter Corporation 
+			of the site. Use of any such linked web site is at the user's own risk.
+		</li>
+	</ul>
+	
+	<ul class="info terms">
+		<li>7. Site Terms of Use Modifications</li>
+		<li>
+			DBA Derby Legleiter Corporation may revise these terms of use for its web site at any time 
+			without notice. By using this web site you are agreeing to be bound by the then current version 
+			of these Terms and Conditions of Use.
+		</li>
+	</ul>
+		
+	<ul class="info terms">
+		<li>8. Governing Law</li>
+		<li>
+			Any claim relating to DBA Derby Legleiter Corporation's web site shall be governed by 
+			the laws of the State of Iowa without regard to its conflict of law provisions.
+		</li>
+	</ul>
+	
+	<ul class="info terms">
+		<li>Note</li>
+		<li>
+			General Terms and Conditions applicable to Use of a Web Site.
+		</li>
+	</ul>
+
+	<h2>
+		Privacy Policy
+	</h2>
+
+
+	<ul class="info terms">
+		<li>General</li>
+		<li>
+			Your privacy is very important to us. Accordingly, we have developed this Policy 
+			in order for you to understand how we collect, use, communicate and disclose and 
+			make use of personal information. The following outlines our privacy policy.
+		</li>
+		<li>
+			Before or at the time of collecting personal information, we will identify the 
+			purposes for which information is being collected.
+		</li>
+		<li>
+			We will collect and use of personal information solely with the objective of 
+			fulfilling those purposes specified by us and for other compatible purposes, unless 
+			we obtain the consent of the individual concerned or as required by law.		
+		</li>
+		<li>
+			We will only retain personal information as long as necessary for the fulfillment 
+			of those purposes. 
+		</li>
+		<li>
+			We will collect personal information by lawful and fair means and, where appropriate, 
+			with the knowledge or consent of the individual concerned. 
+		</li>
+		<li>
+			Personal data should be relevant to the purposes for which it is to be used, and, to 
+			the extent necessary for those purposes, should be accurate, complete, and up-to-date. 
+		</li>
+		<li>
+			We will protect personal information by reasonable security safeguards against loss 
+			or theft, as well as unauthorized access, disclosure, copying, use or modification.
+		</li>
+		<li>
+			We will make readily available to customers information about our policies and practices 
+			relating to the management of personal information. 
+		</li>
+	</ul>
+	
+	<ul class="info terms">
+		<li>Please note</li>
+		<li>
+			We are committed to conducting our business in accordance with these principles in order 
+			to ensure that the confidentiality of personal information is protected and maintained. 
+		</li>
+	</ul>
