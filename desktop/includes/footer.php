4171e272cae466edf64f5a417ee535dd5178f874
Cameron <cameron@cameronl-macbook.student.iastate.edu>
2012-11-16 13:16:11 -0600

    Added stuff for help

--- desktop/includes/footer.php
+++ desktop/includes/footer.php
@@ -1,6 +1,8 @@
 <div class="snack-container"  style="text-align:center">
 	<div><small>
-	    <a href="#">About</a> - <a href="#">Help</a> - <a href="#">Legal</a>
+	    <a href="#" title="About">About</a> - 
+	    <a href="../common/help.php" title="Help" rel="gb_page_center[500, 500]">Help</a> - 
+	    <a href="../common/tos.php" title="Legal - Terms of Service" rel="gb_page_center[500, 500]">Legal</a>
 	</small></div>
 	<div><small class="muted">Copyright 2012 Snack Tack</small></div>
 </div>
